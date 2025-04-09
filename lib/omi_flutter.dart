c39587343763ec82ec4d047b8a4b210d64911790
Devon Crebbin <devon@land.org.au>
2025-04-09 23:33:27 +0100

    fixed connection bug

--- lib/omi_flutter.dart
+++ lib/omi_flutter.dart
@@ -1,47 +1,90 @@
+import 'dart:async';
+
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-
 class OmiFlutter {
-  List<BluetoothDevice> devicesList = [];
+  static BluetoothDevice? connectedDevice;
+  static List<BluetoothDevice> devicesList = [];
 
   /// Scans for Bluetooth devices
   ///
   /// Returns a stream of discovered devices.
   /// The scan will automatically stop after [timeout] duration.
   Future<List<BluetoothDevice>> deviceScan() async {
-    devicesList.clear(); // Clear existing devices before scanning
+    final completer = Completer<void>();
 
-    // Set up the listener before starting the scan
-    FlutterBluePlus.onScanResults.listen((results) {
-      if (results.isNotEmpty) {
-        ScanResult r = results.last; // the most recently found device
-        print('${r.device.remoteId}: "${r.advertisementData.advName}" found!');
-        if (!devicesList.contains(r.device)) {
-          devicesList.add(r.device);
+    var subscription = FlutterBluePlus.onScanResults.listen(
+      (results) {
+        if (results.isNotEmpty) {
+          ScanResult r = results.last;
+          if (!devicesList.contains(r.device)) {
+            devicesList.add(r.device);
+          }
         }
-      }
-    }, onError: (e) => print(e));
+      },
+      onError: (e) {
+        print(e);
+        if (!completer.isCompleted) completer.complete();
+      },
+    );
 
-    // Start scan and wait for the full duration to collect devices
     await FlutterBluePlus.startScan(timeout: Duration(seconds: 4));
-
-    // Wait for the scan to complete
     await FlutterBluePlus.isScanning.where((val) => val == false).first;
+    await Future.delayed(Duration(milliseconds: 100));
+    await subscription.cancel();
 
-    // Return the collected devices
+    print("Devices found: ${devicesList.length}");
     return List<BluetoothDevice>.from(devicesList);
   }
 
-  Future<void> deviceConnect() async {
-    await devicesList[0].connect(autoConnect: true);
+  Future<bool> deviceConnect(String deviceId) async {
+    try {
+      print('Connecting to device $deviceId');
+      print(connectedDevice);
+      DeviceIdentifier deviceIdentifier = DeviceIdentifier(deviceId);
+      if (devicesList.isEmpty) {
+        print('No devices found');
+        return false;
+      }
+      await devicesList
+          .firstWhere((element) => element.remoteId == deviceIdentifier)
+          .connect();
+      connectedDevice = devicesList.firstWhere(
+        (element) => element.remoteId == deviceIdentifier,
+      );
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
   }
 
-  Future<void> deviceDisconnect() async {
-    await devicesList[0].disconnect();
+  Future<bool> deviceDisconnect(String deviceId) async {
+    print('Disconnecting from device $deviceId');
+    try {
+      DeviceIdentifier deviceIdentifier = DeviceIdentifier(deviceId);
+      await devicesList
+          .firstWhere((element) => element.remoteId == deviceIdentifier)
+          .disconnect();
+      connectedDevice = null;
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<List<BluetoothService>> retrieveDeviceInfo() async {
+    if (connectedDevice == null) {
+      return [];
+    }
+    try {
+      var characteristics = await connectedDevice!.discoverServices();
+      print(characteristics);
+      return characteristics;
+    } catch (e) {
+      print(e);
+      return [];
+    }
   }
 }
